644fd384ac682679eb3a754686ac5513539252b2
ajay shankar <ajay.shankar084@gmail.com>
2026-02-28 21:15:07 +0530

    added property

--- src/internship/internship.model.ts
+++ src/internship/internship.model.ts
@@ -6,20 +6,23 @@ export const InternshipSchema = new mongoose.Schema({
   email: { type: String, required: true, minlength: 3 },
   program: { type: String, required: true, minlength: 3 },
   college: { type: String, required: true, minlength: 3 },
-  mobile: { type: String, required: true, minlength: 10,maxLength:10 },
+  mobile: { type: String, required: true, minlength: 10, maxLength: 10 },
   internshipType: { type: String, required: true, minlength: 3 },
   active: { type: Boolean, required: true, default: true },
   createdAt: { type: Date, default: Date.now },
-  
+  referralName: { type: String, required: false, minlength: 3 },
+  referralMobile: { type: String, required: false, minlength: 10, maxLength: 10 },
 });
 
 export interface InternshipModel extends mongoose.Document {
   name: string;
   email: string;
-  program: string; 
+  program: string;
   college: string;
   mobile: string;
   internshipType: string;
   active: boolean;
-  createdAt: Date;  
+  createdAt: Date;
+  referralName: string;
+  referralMobile: string;
 }
